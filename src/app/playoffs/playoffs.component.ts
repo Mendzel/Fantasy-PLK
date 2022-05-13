cc07b34fad40c1398e9a748a88ecab12c0afb822
konmen <konrad.mendzelewski@agora.pl>
2022-05-13 10:55:22 +0200

    Last commit

--- src/app/playoffs/playoffs.component.ts
+++ src/app/playoffs/playoffs.component.ts
@@ -2,55 +2,70 @@ import { Component, OnInit } from '@angular/core';
 import { Subject, Subscription } from 'rxjs';
 import { PlayoffMatchModel } from '../models/playoffMatch.model';
 import { TeamModel } from '../models/team.model';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { DataToSendService } from '../service/data-to-send.service';
 
 @Component({
   selector: 'app-playoffs',
   templateUrl: './playoffs.component.html',
-  styleUrls: ['./playoffs.component.scss']
+  styleUrls: ['./playoffs.component.scss'],
 })
 export class PlayoffsComponent implements OnInit {
-  // teams?: TeamModel[];
-  // fetchingTeam?: Subscription;
-  // dataSent = new Subject();
+  teams?: TeamModel[];
+  fetchingTeam?: Subscription;
+  dataSent = new Subject();
   fetchingMatches?: Subscription;
   playoffMatches: any;
-  constructor(private dataToSend: DataToSendService, private http: HttpClient) { }
+  // playOffMatchToSend: any = [];
+  constructor(
+    private dataToSend: DataToSendService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
     this.dataToSend.getAllPlayoffPairs();
-    this.fetchingMatches = this.dataToSend.playoffMatchesFetched.subscribe((fetchedData: any) => {
-      this.playoffMatches = fetchedData;
-    })
-    // this.dataToSend.getAllTeams();
-    // this.fetchingTeam = this.dataToSend.teamFetched.subscribe((fetchedData: any) => {
-    //   this.teams = fetchedData;
-    //   let index = 0;
-    //   for(let singleMatch of this.playoffMatches[0]){
-    //     let selectedATeam = this.teams?.filter(team => {
-    //       return team.name === singleMatch.team1;
-    //     })
-    //     if(selectedATeam?.[0]){
-    //       singleMatch.team1Data = selectedATeam[0];
-    //       this.playoffMatches[0][index].team1Data = selectedATeam[0]; 
-    //     };
-    //     let selectedBTeam = this.teams?.filter(team => {
-    //       return team.name === singleMatch.team2;
-    //     })
-    //     if(selectedBTeam?.[0]){
-    //       singleMatch.team2Data = selectedBTeam[0]
-    //       this.playoffMatches[0][index].team2Data = selectedBTeam[0]; 
-    //     };
-    //     index++;
-    //   }
-
-    // })
+    this.fetchingMatches = this.dataToSend.playoffMatchesFetched.subscribe(
+      (fetchedData: any) => {
+        this.playoffMatches = fetchedData;
+        // console.log(this.playoffMatches);
+        // this.dataToSend.getAllTeams();
+        // this.fetchingTeam = this.dataToSend.teamFetched.subscribe(
+        //   (fetchedData: any) => {
+        //     this.teams = fetchedData;
+        //     let match: any = [];
+        //     match.push(this.playoffMatches[1][0]);
+        //     match.push(this.playoffMatches[1][1]);
+        //     for (let singleMatch of match) {
+        //       let selectedATeam = this.teams?.filter((team) => {
+        //         return team.name === singleMatch.team1;
+        //       });
+        //       if (selectedATeam?.[0]) {
+        //         singleMatch.team1Data = selectedATeam[0];
+        //       }
+        //       let selectedBTeam = this.teams?.filter((team) => {
+        //         return team.name === singleMatch.team2;
+        //       });
+        //       if (selectedBTeam?.[0]) {
+        //         singleMatch.team2Data = selectedBTeam[0];
+        //       }
+        //       this.playOffMatchToSend.push(singleMatch);
+        //       console.log(singleMatch);
+        //     }
+        //   }
+        // );
+      }
+    );
   }
 
-  // sendData(matches: any) {
-  //   this.http.post('https://fantasy-plk-default-rtdb.europe-west1.firebasedatabase.app/playoffMatches.json', matches).subscribe(responseData => {
-  //     this.dataSent.next(responseData);
-  //   })
+  // sendData(matches: any, key: string) {
+  //   console.log(matches);
+  //   this.http
+  //     .post(
+  //       'https://fantasy-plk-default-rtdb.europe-west1.firebasedatabase.app/playoffMatches/' + key + |,
+  //       matches
+  //     )
+  //     .subscribe((responseData) => {
+  //       this.dataSent.next(responseData);
+  //     });
   // }
 }
